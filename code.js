7287496da8297041c265486f0c0c531d92e6d49d
lucasdellagiustina <161031319+lucasdellagiustina@users.noreply.github.com>
2024-03-01 10:49:12 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const texto = document.body.querySelector("#texto");
+    texto.addEventListener("click", () => {
+            if (texto.textContent === "Olá Mundo")
+                texto.textContent = "Hello Word";
+
+            else
+                texto.textContent = "Olá Mundo";
+        });
+});
 
